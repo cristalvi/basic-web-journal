017acfd3a6f49214ea4101a4b07ffe260c671dc5
Cristal Fernett <creative@cristalfernett.com>
2020-12-08 13:46:37 -0800

    update nav filing system

---
+++ includes/nav2018.php
@@ -0,0 +1,51 @@
+<?php include('includes/nav.php'); ?>
+
+                 <!-- About Me (Left Sidebar) Start -->
+                 <div class="col-md-3">
+                   <div class="about-fixed">
+                    
+                     <div class="my-pic">
+                        <img src="https://i.imgur.com/LcuHYfj.jpg" alt="Lady in a floppy hat">
+<!--                        <img src="images/pic/my-pic.png" alt=""> -->
+                        <a href="javascript:void(0)" class="collapsed" data-target="#menu" data-toggle="collapse"><i class="icon-menu menu"></i></a>
+                         <div id="menu" class="collapse">
+                           <ul class="menu-link">
+                            <h4><a href="index_December.html">2017</a>&nbsp;&nbsp;&nbsp;
+                              <strong>2018</strong>
+                              <a href="index_December2019.html">2019</a></h4>
+                                   <li><a href="index_January2018.html">January</a></li>
+                                   <li><a href="index_February2018.html">February</a></li>
+                                   <li><a href="index_March2018.html">March</a></li>
+                                   <li><a href="index_April2018.html">April</a></li>
+                                   <li><a href="index_May2018.html">May</a></li>
+                                   <li><a href="index_June2018.html">June</a></li>
+                                   <li><a href="index_July2018.html">July</a></li>
+                                   <li><a href="index_August2018.html">August</a></li>
+                                   <li><a href="index_September2018.html">September</a></li>
+                                   <li><a href="index_October2018.html">October</a></li>
+                                   <li><a href="index_November2018.html">November</a></li>
+                                   <li><a href="index_December2018.html">December</a></li>
+                            </ul>
+                         </div>
+                        </div>
+                      
+                      
+                      
+                      <div class="my-detail">
+                    	
+                        <div class="white-spacing">
+                            <h1>My Time Here</h1>
+                            <span>An account of my thoughts<br/>and experiences, and other<br/>day to day happenings</span>
+                        </div> 
+                       
+                       <ul class="social-icon">
+                         <li><a href="http://www.facebook.com/cristalvisays" target="_blank" class="facebook"><i class="fa fa-facebook"></i></a></li>
+                         <li><a href="http://www.twitter.com/cristalvisays" target="_blank" class="twitter"><i class="fa fa-twitter"></i></a></li>
+                         <li><a href="http://www.linkedin.com/in/cristalvi" target="_blank" class="linkedin"><i class="fa fa-linkedin"></i></a></li>
+                         <li><a href="https://github.com/cristalvi" target="_blank" class="github"><i class="fa fa-github"></i></a></li>
+                        </ul>
+
+                    </div>
+                  </div>
+                </div>
+                <!-- About Me (Left Sidebar) End -->
